9b41120eb793dee6ae23c23b7c9328d888467d04
REVATHY <revathy.chinnu19@gmail.com>
2024-10-08 16:53:39 +0530

    commit it

---
+++ project25/src/pro25/Project25.java
@@ -0,0 +1,15 @@
+package pro25;
+
+public class Project25 
+{
+	
+public static void main(String[] args) {
+	int age= 30;
+	if(age>18)
+	{
+		System.out.println("Age is greater than 18");
+	}
+
+}
+
+}
